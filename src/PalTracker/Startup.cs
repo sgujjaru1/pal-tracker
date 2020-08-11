1b8aa64af7af5d2a042d5ba4d480ab0f1d05e97a
sgujjaru1 <sgujjaru1@gmail.com>
2020-08-11 13:11:07 +0530

    time test

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -34,6 +34,7 @@ namespace PalTracker
             throw new ApplicationException("WELCOME_MESSAGE not configured.");
             }
             services.AddSingleton(sp => new WelcomeMessage(message));
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
